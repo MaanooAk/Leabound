7cb7dccd0a64729f2fc7a6bf6dcf9a023b00342a
MaanooAk <akritasak@gmail.com>
2019-11-26 11:27:40 +0200

    Dispenser thing

---
+++ core/src/com/maanoo/leabound/core/thing/Dispenser.java
@@ -0,0 +1,37 @@
+package com.maanoo.leabound.core.thing;
+
+import com.maanoo.leabound.core.board.Board;
+import com.maanoo.leabound.core.util.Direction;
+import com.maanoo.leabound.core.util.Location;
+
+
+public class Dispenser extends StateThing {
+
+	private static final SimpleState Idle = new SimpleState("dispenser_idle", true);
+	private static final SimpleState Used = new SimpleState("dispenser_used", true);
+
+	private final Board board;
+	private final PickUp dispenseObject;
+
+	public Dispenser(Location location, Direction rotation, Board board, PickUp dispenseObject) {
+		super("Dispenser", location, rotation, Idle, Used);
+		this.board = board;
+		this.dispenseObject = dispenseObject;
+	}
+
+	@Override
+	public boolean onThingActiveChange(Thing thing) {
+		if (thing == this) return false;
+
+		if (getState() == Used) return false;
+
+		if (!thing.isActive()) return false;
+		if (!activeFlow(thing.getLocation())) return false;
+
+		dispenseObject.getLocation().set(getLocation()).add(getRotation().vector);
+		board.addThing(dispenseObject);
+
+		return nextState();
+	}
+
+}
