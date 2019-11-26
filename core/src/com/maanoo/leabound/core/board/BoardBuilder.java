138418e8d83019895b54a204cff6335f9387ca03
MaanooAk <akritasak@gmail.com>
2019-11-26 11:55:20 +0200

    Fixed boards and board buidler

---
+++ core/src/com/maanoo/leabound/core/board/BoardBuilder.java
@@ -0,0 +1,170 @@
+package com.maanoo.leabound.core.board;
+
+import com.badlogic.gdx.utils.Align;
+import com.maanoo.leabound.core.Player;
+import com.maanoo.leabound.core.item.Item;
+import com.maanoo.leabound.core.thing.AndGate;
+import com.maanoo.leabound.core.thing.Dispenser;
+import com.maanoo.leabound.core.thing.Door;
+import com.maanoo.leabound.core.thing.LatchGate;
+import com.maanoo.leabound.core.thing.LockedChest;
+import com.maanoo.leabound.core.thing.NotGate;
+import com.maanoo.leabound.core.thing.OnePassDoor;
+import com.maanoo.leabound.core.thing.PickUp;
+import com.maanoo.leabound.core.thing.PressurePlate;
+import com.maanoo.leabound.core.thing.Switch;
+import com.maanoo.leabound.core.thing.Thing;
+import com.maanoo.leabound.core.thing.Wall;
+import com.maanoo.leabound.core.thing.Wire;
+import com.maanoo.leabound.core.util.Direction;
+import com.maanoo.leabound.core.util.Ra;
+import com.maanoo.leabound.core.util.Location;
+
+
+public class BoardBuilder {
+
+	private static final int BoardWidth = 16;
+	private static final int BoardHeight = 14;
+
+	// === class ===
+
+	private String name;
+	private String board;
+	private String[] things;
+
+	public BoardBuilder(String name, String board, String... things) {
+		this.name = name;
+		this.board = board;
+		this.things = things;
+	}
+
+	public Board build(Player player) {
+
+		final Board b = new Board(name, new Bound(BoardWidth, BoardHeight, player));
+
+		Location location = new Location(-8, 6);
+
+		for (final char c : board.toCharArray()) {
+
+			if (c == '\n') {
+				location.set(-8, location.y - 1);
+				continue;
+			}
+
+			if (c == ' ') {
+				// no op
+			} else if (c == 'X') {
+				b.addThing(new Wall(location));
+				location = location.cpy();
+			} else if (c == 'W' || c == '-') {
+				b.addThing(new Wire(location));
+				location = location.cpy();
+			} else if (c == 'P') {
+				b.addThing(new PressurePlate(location));
+				location = location.cpy();
+			} else if (c == 'S') {
+				b.addThing(new Switch(location));
+				location = location.cpy();
+			} else if (c == 'O') {
+				b.addThing(new OnePassDoor(location));
+				location = location.cpy();
+			} else if (c == 'A') {
+				b.addThing(new AndGate(location, Direction.Down));
+				location = location.cpy();
+			} else if (c == 'a') {
+				b.addThing(new AndGate(location, Direction.Up));
+				location = location.cpy();
+			} else if (c == 'N') {
+				b.addThing(new NotGate(location, Direction.Down));
+				location = location.cpy();
+			} else if (c == 'L') {
+				b.addThing(new LatchGate(location, Direction.Down));
+				location = location.cpy();
+			} else {
+
+				final Thing thing = parseThing(findThingDefiniton(c, things), b, location);
+				if (thing != null) {
+
+					b.addThing(thing);
+					location = location.cpy();
+				}
+			}
+			location.add(1, 0);
+		}
+
+		b.onCreate();
+
+		return b;
+	}
+
+	private static String findThingDefiniton(char c, String[] things) {
+
+		for (int i = 0; i < things.length; i += 2) {
+			if (things[i].charAt(0) == c) return things[i + 1];
+		}
+
+		throw new RuntimeException("Unkown thing definition: " + c);
+	}
+
+	private static Thing parseThing(String text, Board board, Location location) {
+
+		final String[] parts = text.split("[ ]+");
+
+		if (parts[0].equals("dispenser")) {
+			final Direction rotation = parseRotation(parts[1]);
+			final Item item = parseItem(parts[2]);
+
+			return new Dispenser(location, rotation, board,
+					new PickUp(null, item, rotation.origin));
+
+		} else if (parts[0].equals("locked-chest")) {
+			final Item unlock = parseItem(parts[1]);
+			final Item item = parseItem(parts[2]);
+
+			return new LockedChest(location, unlock,
+					board, new PickUp(null, item, Align.center));
+
+		} else if (parts[0].equals("door")) {
+			final Item unlock = parseItem(parts[1]);
+
+			return new Door(location, unlock);
+
+		} else if (parts[0].equals("pick-up")) {
+			final Item item = parseItem(parts[1]);
+
+			return new PickUp(location, item, Align.center);
+
+		} else if (parts[0].equals("wall")) {
+			final float chance = Integer.parseInt(parts[1]) / 100f;
+
+			if (!Ra.chance(chance)) return null;
+
+			return new Wall(location);
+
+		} else if (parts[0].equals("and-gate")) {
+			final Direction rotation = parseRotation(parts[1]);
+
+			return new AndGate(location, rotation);
+
+		} else {
+			throw new RuntimeException("Unknown thing: " + parts[0]);
+		}
+
+	}
+
+	private static Item parseItem(String text) {
+
+		return Item.get(text);
+	}
+
+	private static Direction parseRotation(String text) {
+
+		if (text.equals("r") || text.equals(">")) return Direction.Right;
+		if (text.equals("d") || text.equals("V")) return Direction.Down;
+		if (text.equals("l") || text.equals("<")) return Direction.Left;
+		if (text.equals("u") || text.equals("^")) return Direction.Up;
+
+		throw new RuntimeException(text);
+	}
+
+}
---
+++ core/src/com/maanoo/leabound/core/board/Boards.java
@@ -0,0 +1,233 @@
+package com.maanoo.leabound.core.board;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import com.maanoo.leabound.core.Player;
+
+public class Boards {
+
+	public static final HashMap<Integer, BoardBuilder> fixed = new HashMap<Integer, BoardBuilder>();
+	static {
+
+		fixed.put(1, new BoardBuilder(
+				"!Pick up the [part]part[] and then [leap]leap[] again" ,""
+				+ "Xxxxx           \n"
+				+ "xx          x   \n"
+				+ "            Xx  \n"
+				+ "   1            \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "               X\n"
+				+ "               X\n"
+				+ "                \n"
+				+ "  Xx            \n"
+				+ "  x            X\n"
+				+ "X             XX\n"
+				+ "XX          xxxx\n"
+
+				, "1", "pick-up parts"
+				, "x", "wall 50"
+
+		));
+
+		fixed.put(2, new BoardBuilder(
+				"!Pick up the [part]key[] and open the [chest]chest[]" ,""
+				+ "XXXXX      xxXXX\n"
+				+ "xx            xX\n"
+				+ "                \n"
+				+ "   2            \n"
+				+ "                \n"
+				+ "            1   \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "XXX            x\n"
+				+ "x              X\n"
+				+ "x              X\n"
+				+ "XXxx            \n"
+				+ "XXXXxxxx        \n"
+
+				, "1", "pick-up key"
+				, "2", "locked-chest key parts"
+				, "x", "wall 50"
+
+		));
+
+		fixed.put(3, new BoardBuilder(
+				"!Go over the [part]pressure plate[]", ""
+				+ "XXxxxxx      xxX\n"
+				+ "xxxxx         xx\n"
+				+ "xxx            x\n"
+				+ "x              x\n"
+				+ "         1--   x\n"
+				+ "           -    \n"
+				+ "           -    \n"
+				+ "           -    \n"
+				+ "           -    \n"
+				+ "   P--------    \n"
+				+ "xx              \n"
+				+ "xx           xxx\n"
+				+ "xxxx        xxxX\n"
+				+ "XXxxxxxxxxxxxxXX\n"
+
+				, "1", "dispenser l parts"
+				, "x", "wall 50"
+
+		));
+
+		fixed.put(4, new BoardBuilder(
+				"!Flick the two [part]switches[]", ""
+				+ "XXxxxxx      xxX\n"
+				+ "xxxxx         xx\n"
+				+ "xxx            x\n"
+				+ "x              x\n"
+				+ "       -1      x\n"
+				+ "       -        \n"
+				+ "       -        \n"
+				+ "   S---2        \n"
+				+ "       -        \n"
+				+ "       -        \n"
+				+ "xx   S--        \n"
+				+ "xx           xxx\n"
+				+ "xxxx        xxxX\n"
+				+ "XXxxxxxxxxxxxxXX\n"
+
+				, "1", "dispenser r parts"
+				, "2", "and-gate u"
+				, "x", "wall 50"
+
+		));
+
+		fixed.put(5, new BoardBuilder(
+				"!You can pass over the [part]??[] once", ""
+				+ "XXxxxxXxxxxxxxxx\n"
+				+ "x     Xxx    xxx\n"
+				+ "x  1  X         \n"
+				+ "x     O         \n"
+				+ "XXXXXOX         \n"
+				+ "xxxx        xxxx\n"
+				+ "         XXXXXXX\n"
+				+ "         O     x\n"
+				+ "         X  2  x\n"
+				+ "         XXXXXXX\n"
+				+ "x          xxxxx\n"
+				+ "Xx              \n"
+				+ "Xxx             \n"
+				+ "XXXx            \n"
+
+				, "1", "pick-up key"
+				, "2", "locked-chest key parts"
+				, "x", "wall 50"
+
+		));
+
+
+	}
+
+	private static final ArrayList<BoardBuilder> builders = new ArrayList<BoardBuilder>();
+	static {
+
+		builders.add(new BoardBuilder("Empty", ""
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+		));
+		builders.clear(); // ???
+
+		builders.add(new BoardBuilder("Wires", ""
+				+ "XX              \n"
+				+ "X               \n"
+				+ "  PWWWWk     P  \n"
+				+ "      W      WWL\n"
+				+ "      AWWS   N W\n"
+				+ "      W      W W\n"
+				+ "      Wb        \n"
+				+ "  2      S S S  \n"
+				+ "         W W W  \n"
+				+ "  3      WAWAW  \n"
+				+ "          W W   \n"
+				+ "XXdX      WAW   \n"
+				+ "XppX       c    \n"
+				+ "XXXX            \n"
+
+				, "k", "dispenser u key"
+				, "b", "dispenser d key"
+				, "c", "dispenser r key"
+				, "2", "locked-chest key master-key"
+				, "3", "locked-chest master-key parts"
+				, "d", "door key"
+				, "p", "pick-up parts"
+		));
+
+		builders.add(new BoardBuilder("Wires", ""
+				+ "X              X\n"
+				+ " X            X \n"
+				+ "  p          p  \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "        1       \n"
+				+ "        -       \n"
+				+ "  S-----a----S  \n"
+				+ " X            X \n"
+				+ "X              X\n"
+
+				, "1", "dispenser u master-key"
+				, "p", "pick-up parts"
+		));
+
+		builders.add(new BoardBuilder("Wires", ""
+				+ "                \n"
+				+ "                \n"
+				+ " 1  2  3  4  5  \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ " 6  7  8  9  0  \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+				+ "                \n"
+
+				, "1", "pick-up key"
+				, "2", "pick-up key-a"
+				, "3", "pick-up key-b"
+				, "4", "pick-up key-c"
+				, "5", "pick-up master-key"
+
+				, "6", "locked-chest key parts"
+				, "7", "locked-chest key-a parts"
+				, "8", "door key-b"
+				, "9", "door key-c"
+				, "0", "locked-chest master-key parts"
+		));
+
+	}
+
+	public static Board get(Player player) {
+
+		return builders.get(new Random().nextInt(builders.size())).build(player);
+	}
+
+
+}
