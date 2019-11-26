6f5bd567a16ad8f39627d1459bd85d888c795879
MaanooAk <akritasak@gmail.com>
2019-11-26 11:23:19 +0200

    Fake Wall thing

---
+++ core/src/com/maanoo/leabound/core/thing/FakeWall.java
@@ -0,0 +1,54 @@
+package com.maanoo.leabound.core.thing;
+
+import com.badlogic.gdx.utils.Align;
+import com.maanoo.leabound.core.Player;
+import com.maanoo.leabound.core.item.Item;
+import com.maanoo.leabound.core.util.Direction;
+import com.maanoo.leabound.core.util.Location;
+
+
+public class FakeWall extends Thing {
+
+	// TODO crate abstract of health kind things
+
+	private final String[] drawables;
+	private final int health;
+
+	private int damage;
+
+	public FakeWall(Location location) {
+		super("Fake Wall", location, Direction.random());
+
+		drawables = new String[] {
+				"fake-wall", "fake-wall_1", "fake-wall_2", "fake-wall_3",
+		};
+
+		health = drawables.length;
+		damage = 0;
+	}
+
+	@Override
+	public boolean isBlocking() {
+		return true;
+	}
+
+	@Override
+	public boolean onPlayerBounce(Player player) {
+		damage += 1;
+
+		if (damage == health) {
+			destroy();
+
+			player.getBoard().addThing(new PickUp(getLocation().cpy(), Item.Parts, Align.center));
+		}
+
+		mod();
+		return true;
+	}
+
+	@Override
+	public String getDrawable() {
+		return drawables[damage];
+	}
+
+}
